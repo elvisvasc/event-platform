7aef98daad385676e1fe3cde3533137fe0daa065
Evis de Vasconcelos da Cunha <elvisvasc@bb.com.br>
2022-06-24 10:43:36 -0300

    Criação da pagina home e inclusao da variaveis de ambiente para permitir usar o token de acesso para fazer as mutations no graphcms.

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oq0yww0o3b01yw8fh06u9e/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
